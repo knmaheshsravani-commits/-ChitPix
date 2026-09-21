d310057b1c2fe90d2c21cca3f1afc148ff61fd5e
knmaheshsravani-commits <knmaheshsravani@gmail.com>
2026-09-22 00:14:38 +0530

    Change default name from 'knmahesh' to 'mahesh'

--- app/page.tsx
+++ app/page.tsx
@@ -95,7 +95,7 @@ export default function Page() {
     );
   }
 
-  const myname = session.user.email?.split("@")[0] || "knmahesh";
+  const myname = session.user.email?.split("@")[0] || "mahesh";
 
   return (
     <div className="min-h-screen bg-black text-white pb-24">
